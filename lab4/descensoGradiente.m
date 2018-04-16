f797949935c0722937273a91840b5452b184eef4
Diego Calero <diegoacalero0@gmail.com>
2018-04-16 13:41:43 -0500

    Avance lab4

--- lab4/descensoGradiente.m
+++ lab4/descensoGradiente.m
@@ -1,12 +1,14 @@
 function [theta J_historia]=descensoGradiente(X,y,alpha,num_iter)
   theta=[0;0;0];
   h=sigmoide(X*theta);
-  J_historia(1)=costo(y,h);
 
+  m = length(y);
+  
   for i=1:num_iter
-    g=(1/length(y))*((h-y)'*X)';
+    g=(1/m)*((h-y)'*X)';
     theta=theta-alpha*g;
     h=sigmoide(X*theta);
-    J_historia(i+1)=costo(y,h);
+    J_historia(i) = costo(y, h);
   endfor
+  J_historia = J_historia';
 end
---
+++ lab4/graficarTasas.m
@@ -0,0 +1,73 @@
+function [] = graficarTasas(X, y)
+  
+  title("Costo vs numero de iteracion para varias tasas de aprendizaje");
+  xlabel("iteracion");
+  ylabel("funcion costo");
+  
+  
+  [theta arr] = descensoGradiente(X, y, 1000, 50); 
+  hold off
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 300, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 100, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 30, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 10, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 3, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 1, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.3, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.1, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.03, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.01, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.003, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.001, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.0003, 50); 
+  hold on
+  plot(arr);
+  
+  [theta arr] = descensoGradiente(X, y, 0.0001, 50); 
+  hold on
+  plot(arr);
+  
+  legend("alpha = 1000", "alpha = 300", "alpha = 100", "alpha = 30", "alpha = 10", "alpha = 3"
+  , "alpha = 1"
+  , "alpha = 0.3", "alpha = 0.1", "alpha = 0.03", "alpha = 0.01"
+  , "alpha = 0.003", "alpha = 0.001", "alpha = 0.0003", "alpha = 0.0001");
+  
+endfunction
